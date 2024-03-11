76f466979f18d5574e7f0d9f7de3bea1a53a21cd
maksim t <maksim.teslenko.dev@gmail.com>
2024-03-11 23:31:56 +0200

    init project, add npm,prettier,linter,prisma,ts,config

---
+++ config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  static: {
+    port: 8000,
+  },
+  api: {
+    port: 8001,
+    transport: 'http',
+  },
+  sandbox: {
+    timeout: 5000,
+    displayErrors: false,
+  },
+  db: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'BookingHotelsDB',
+    user: 'posgres',
+    password: 'root',
+  },
+  pg: {
+    database: 'postgres',
+    user: 'postgres',
+    password: 'root',
+  },
+};
